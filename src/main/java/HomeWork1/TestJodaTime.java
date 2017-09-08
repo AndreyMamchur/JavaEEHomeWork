d91f258782840036a6f706a6b42d9a9ce3b787c5
AndreyMamchur <mamchurandrey87@gmail.com>
2017-09-08 12:53:35 +0300

    HomeWork #1 Create new dependency and test how they work.

---
+++ src/main/java/HomeWork1/TestJodaTime.java
@@ -0,0 +1,11 @@
+package HomeWork1;
+
+import org.joda.time.LocalDate;
+
+public class TestJodaTime {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
+
